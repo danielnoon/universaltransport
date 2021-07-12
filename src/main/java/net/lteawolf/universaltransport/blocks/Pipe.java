77e576e648a9577138e8a1e7be291e1d22190ce8
Daniel Noon <danielanoon@outlook.com>
2021-07-12 17:53:18 -0500

    initial commit:

---
+++ src/main/java/net/lteawolf/universaltransport/blocks/Pipe.java
@@ -0,0 +1,9 @@
+package net.lteawolf.universaltransport.blocks;
+
+import net.minecraft.block.Block;
+
+public class Pipe extends Block {
+  public Pipe(Properties properties) {
+    super(properties);
+  }
+}
